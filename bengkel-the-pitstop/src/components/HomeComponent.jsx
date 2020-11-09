108110e630c771af44f4bbe3481ff75e0160c8b9
Jeremy Kane <bob@example.com>
2020-11-09 17:15:07 +0700

    added card layout for the brief description of the company with adaptive layout

--- bengkel-the-pitstop/src/components/HomeComponent.jsx
+++ bengkel-the-pitstop/src/components/HomeComponent.jsx
@@ -15,28 +15,32 @@ class HomeComponent extends Component {
 
         return (
             <div className = "content">
-                <h1> Bengkel The Pitstop </h1>
-                <ul>
-                  <li>
-                    <div>
-                      <p>
-                        <strong> BENGKEL SEGALA JENIS MOBIL </strong>
-                        <span> Jl. Letjen Harun Sohar No. 9 (Simpang Bandara SMB II) </span>
-                      </p>
-                    </div>
+              <div className = "card">
+                <div className = "card-body">
+                  <h1 className = "card-title"> Bengkel The Pitstop </h1>
+                  <ul>
+                    <li>
+                      <div>
+                        <p className = "card-text">
+                          <strong> BENGKEL SEGALA JENIS MOBIL </strong>
+                          <span> Jl. Letjen Harun Sohar No. 9 (Simpang Bandara SMB II) </span>
+                        </p>
+                      </div>
 
-                    <div>
-                      <p>
-                        <span>
-                          <strong> Phone:</strong> (0711) 5710200 / 082175311779
-                          <strong> Instagram:</strong> @bengkelthepitstop
-                          <strong> Link Ford-RMA:</strong> <a href="http://ford-rma.co.id/id/pusat-layanan-ford-resmi/">http://ford-rma.co.id/id/pusat-layanan-ford-resmi/</a>
+                      <div>
+                        <p className = "card-text">
+                          <span>
+                            <strong> Phone:</strong> (0711) 5710200 / 082175311779
+                            <strong> Instagram:</strong> @bengkelthepitstop
+                            <strong> Link Ford-RMA:</strong> <a href="http://ford-rma.co.id/id/pusat-layanan-ford-resmi/">http://ford-rma.co.id/id/pusat-layanan-ford-resmi/</a>
 
-                        </span>
-                      </p>
-                    </div>
-                  </li>
-                </ul>
+                          </span>
+                        </p>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
         )}
 }

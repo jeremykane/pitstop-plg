fe07d978ff04b2b22ed03c03e1ff4c7bee0acb24
Jeremy Kane <jeremykane1579@gmail.com>
2020-11-11 10:16:53 +0700

    added working carousel and try to fit a logo in the col1

--- bengkel-the-pitstop/src/components/HomeComponent.jsx
+++ bengkel-the-pitstop/src/components/HomeComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../styles/HomeComponentStyling.css"
 import Carousel from 'react-bootstrap/Carousel'
+import PitstopImage from "../assets/pitstop-lg.jpeg"
 class HomeComponent extends Component {
 
     constructor(props) {
@@ -8,10 +9,14 @@ class HomeComponent extends Component {
         this.state = {
             data : [],
             index : 0,
-            
+
         }
     }
-
+    
+    handleSelect = e => {
+      console.log(e)
+    }
+    
     render () {
 
         return (
@@ -39,51 +44,53 @@ class HomeComponent extends Component {
                       </ul>
                     </div>
                   </div>
+                  <div>
+                    <img src = "https://www.fitnessfirst.com.au/assets/img/brand/ff-logo-large.png"></img>
+                  </div>
                 </div>
                 <div className = "mycol col2">
+                  <Carousel className = "myCarousel">
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src= {PitstopImage}
+                        alt="First slide"
+                      />
+                      <Carousel.Caption>
+                        <h3>First Image</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src= {PitstopImage}
+                        alt="Third slide"
+                      />
+
+                      <Carousel.Caption>
+                        <h3>Second Image</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src= {PitstopImage}
+                        alt="Third slide"
+                      />
+
+                      <Carousel.Caption>
+                        <h3>Third Image</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
                 </div>
               </div>
               <hr/>
               <div>
-                <Carousel activeIndex={this.state.index} onSelect={handleSelect}>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=First slide&bg=373940"
-                      alt="First slide"
-                    />
-                    <Carousel.Caption>
-                      <h3>First slide label</h3>
-                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=Second slide&bg=282c34"
-                      alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                      <h3>Second slide label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src="holder.js/800x400?text=Third slide&bg=20232a"
-                      alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                      <h3>Third slide label</h3>
-                      <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                      </p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                </Carousel>
+              
               </div>
             </div>
           </div>

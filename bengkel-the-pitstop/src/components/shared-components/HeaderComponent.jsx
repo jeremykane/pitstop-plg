5323ecf438216d0fecbc5dbff88f65397af6e6d5
Jeremy Kane <jeremykane1579@gmail.com>
2020-11-16 17:01:05 +0700

    Tried to use less css to give animation for the toggler but not working yet (codes are commented out)

--- bengkel-the-pitstop/src/components/shared-components/HeaderComponent.jsx
+++ bengkel-the-pitstop/src/components/shared-components/HeaderComponent.jsx
@@ -39,7 +39,7 @@ class HeaderComponent extends Component {
                       
                     </Nav>
 
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                         <Nav.Link href="/home" className="nav-bar-button">Home</Nav.Link>
@@ -49,6 +49,52 @@ class HeaderComponent extends Component {
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
+                {/* <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar top-bar"></span>
+                    <span class="icon-bar middle-bar"></span>
+                    <span class="icon-bar bottom-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Company</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="about.html">About Us</a>
+                            </li>
+                            <li>
+                                <a href="certification.html">Certification</a>
+                            </li>
+                            <li>
+                                <a href="downloads.html">Download PDF</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="products.html">Products</a>
+                    </li>
+                    <li>
+                        <a href="inquiry.html">Inquiry</a>
+                    </li>
+                    <li>
+                        <a href="events.html">Events</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> */}
             </header>
         )
     }

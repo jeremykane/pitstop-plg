2d39f13db55bbf5ad2bd29d07bf8f21b70785f67
Jeremy Kane <bob@example.com>
2020-11-20 16:05:52 +0700

    Fixed inner html error

--- bengkel-the-pitstop/src/components/PromoComponent.jsx
+++ bengkel-the-pitstop/src/components/PromoComponent.jsx
@@ -7,29 +7,29 @@ import Promo1 from '../assets/promo1.png'
 import Promo2 from '../assets/promo2.png'
 import Promo3 from '../assets/promo3.png'
 
-class PromoComponent extends Component{
-    render(){
-        return(
+class PromoComponent extends Component {
+    render() {
+        return (
             <div>
-                {/* <OwlCarousel className="owl-theme" items="2" autoplay nav loop>
-                 <div className = "item">
-                     <img src={Promo1} alt="promo1">
-                         <p>Test</p>
-                     </img>
-                 </div>
-                 <div className = "item">
-                     <img src={Promo2} alt="promo2">
-                         <p>Test</p>
-                     </img>
-                 </div>
-                 <div className = "item">
-                     <img src={Promo3} alt="promo3">
-                         <p>Test</p>
-                     </img>
-                 </div>
-            </OwlCarousel> */}
-            </div>
-            
+                <OwlCarousel className="owl-theme" items="2" autoplay nav>
+                    <div className="item">
+                        <img src={Promo1} alt="promo1">
+                        </img>
+                        <p>Test</p>
+                    </div>
+                    <div className="item">
+                        <img src={Promo2} alt="promo2">
+                        </img>
+                        <p>Test</p>
+                    </div>
+                    <div className="item">
+                        <img src={Promo3} alt="promo3">
+                        </img>
+                        <p>Test</p>
+                    </div>
+                </OwlCarousel>
+            </div >
+
         );
     }
 }

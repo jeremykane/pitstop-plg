da7f04fffbf5e92d7b407c6f3e3ac6c8bda21919
Jeremy Kane <s3700178@student.rmit.edu.au>
2020-12-26 11:22:37 +0700

    made some changes for the contact us content

--- bengkel-the-pitstop/src/components/ContactUsComponent.jsx
+++ bengkel-the-pitstop/src/components/ContactUsComponent.jsx
@@ -22,31 +22,40 @@ class ContactUsComponent extends Component {
                         <p className="lead">Contact Details and Working Hours</p>
                     </Container>
                 </Jumbotron>
-                <Row className="justify-content-md-center">
+                <Row className="justify-content-md-center contact-row">
                     <Col>
-                        <Card className="text-center working-hours">
-                            <Card.Body>
-                                <Card.Title>
-                                    <h2>Opening Hours :</h2>
-                                </Card.Title>
-                                <Table responsive="lg" className="working-hours-table table">
-                                    <thead></thead>
-                                    <tbody>
-                                        <tr><th>Monday</th><td>8:30am - 5pm</td></tr>
-                                        <tr><th>Tuesday</th><td>8:30am - 5pm</td></tr>
-                                        <tr><th>Wednesday</th><td>8:30am - 5pm</td></tr>
-                                        <tr><th>Thursday</th><td>8:30am - 5pm</td></tr>
-                                        <tr><th>Friday</th><td>8:30am - 5pm</td></tr>
-                                        <tr><th>Saturday</th><td>9am - 4pm</td></tr>
-                                        <tr><th>Sunday</th><td>Closed</td></tr>
-                                    </tbody>
-                                </Table>
-                                <Card.Title>
-                                    Reservation Via:
-                                </Card.Title>
-                                <a className="fab fa-whatsapp" href="/"><span className="hidden">a</span></a>0821-7531-1779
-                            </Card.Body>
-                        </Card>
+                    <Card>
+                        <Card.Title>
+                        <h2>Opening Hours :</h2>
+                        </Card.Title>
+                        <Card.Body>
+                            <Table responsive="lg" className="working-hours-table table">
+                                <thead></thead>
+                                <tbody>
+                                    <tr><th>Monday</th><td>8:30am - 5pm</td></tr>
+                                    <tr><th>Tuesday</th><td>8:30am - 5pm</td></tr>
+                                    <tr><th>Wednesday</th><td>8:30am - 5pm</td></tr>
+                                    <tr><th>Thursday</th><td>8:30am - 5pm</td></tr>
+                                    <tr><th>Friday</th><td>8:30am - 5pm</td></tr>
+                                    <tr><th>Saturday</th><td>9am - 4pm</td></tr>
+                                    <tr><th>Sunday</th><td>Closed</td></tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>       
+                    </Card>
+                    <Card>
+                        <Card.Title>
+                            <h3>Reservation Via:</h3>
+                        </Card.Title>
+                        <Card.Body>
+                            <a className="fab fa-whatsapp" href="/https://wa.me/+6282175311779"><span className="hidden">a</span></a>0821-7531-1779
+                        </Card.Body>
+                   
+                                
+                            
+                    </Card>
+                                    
+                                    
                     </Col>
                 </Row>
             </div >
